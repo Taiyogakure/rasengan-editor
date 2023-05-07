f9a8f1b0d86e34af90c20a2f18cfe2c369d6ca06
Azanul <azanulhaque@gmail.com>
2023-05-07 18:12:00 +0530

    devcontainer setup
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ .devcontainer/init.sh
@@ -0,0 +1,5 @@
+#Install Buffalo
+wget https://github.com/gobuffalo/cli/releases/download/v0.18.14/buffalo_0.18.14_Linux_x86_64.tar.gz
+tar -xvzf buffalo_0.18.14_Linux_x86_64.tar.gz
+sudo mv buffalo /usr/local/bin/buffalo
+sudo rm buffalo_0.18.14_Linux_x86_64.tar.gz
